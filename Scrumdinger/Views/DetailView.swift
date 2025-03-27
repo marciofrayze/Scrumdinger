f9217d5efc4cc6de1bf1f1ca65887efbe0d24cd3
Marcio Frayze David <marcio.david@serpro.gov.br>
2025-03-27 11:53:22 -0300

    Adds create and update daily scrum features

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -1,4 +1,3 @@
-
 import SwiftUI
 
 
@@ -47,21 +46,10 @@ struct DetailView: View {
         }
         .sheet(isPresented: $isPresentingEditView) {
             NavigationStack {
-                DetailEditView(scrum: $editingScrum)
-                    .navigationTitle(scrum.title)
-                    .toolbar {
-                        ToolbarItem(placement: .cancellationAction) {
-                            Button("Cancel") {
-                                isPresentingEditView = false
-                            }
-                        }
-                        ToolbarItem(placement: .confirmationAction) {
-                            Button("Done") {
-                                isPresentingEditView = false
-                                scrum = editingScrum
-                            }
-                        }
-                    }
+                DetailEditView(scrum: $editingScrum, saveEdits: { dailyScrum in
+                    scrum = editingScrum
+                })
+                .navigationTitle(scrum.title)
             }
         }
     }
--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -1,10 +1,11 @@
-
 import SwiftUI
 
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
-  
+    @State private var isPresentingNewScrumView = false
+
+
     var body: some View {
         NavigationStack {
             List($scrums) { $scrum in
@@ -15,11 +16,16 @@ struct ScrumsView: View {
             }
             .navigationTitle("Daily Scrums")
             .toolbar {
-                Button(action: {}) {
+                Button(action: {
+                    isPresentingNewScrumView = true
+                }) {
                     Image(systemName: "plus")
                 }
                 .accessibilityLabel("New Scrum")
             }
+        }
+        .sheet(isPresented: $isPresentingNewScrumView) {
+            NewScrumSheet(scrums: $scrums)
         }
     }
 }
